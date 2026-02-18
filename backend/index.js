1d3b428fe168b1f35f9dbefd1df3d252b15ef04c
fahad khan <97621401+fahadkhan001@users.noreply.github.com>
2026-02-18 16:16:08 +0530

    feat: enhance server setup with static file serving and update package scripts

--- backend/index.js
+++ backend/index.js
@@ -2,7 +2,7 @@ import express from 'express';
 import connectDB  from './config/db.js';
 import dotenv from "dotenv";
 import cors from "cors";
-
+import path from 'path';
 
 import authRoutes from "./routes/authRoutes.js";
 import timesheetRoutes from "./routes/timesheetRoutes.js";
@@ -11,6 +11,8 @@ import userRoutes from "./routes/userRoutes.js";
 dotenv.config();
 connectDB();
 
+
+const __dirname= path.resolve();
 const app = express();
 app.use(cors());
 app.use(express.json());  
@@ -18,6 +20,14 @@ app.use(express.json());
 app.use("/auth", authRoutes);
 app.use("/timesheets", timesheetRoutes);
 app.use("/users", userRoutes);
+
+app.use(express.static(path.join(__dirname, 'client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client','dist', 'index.html'));
+}
+
+
 
 app.get("/", (req, res) => {
   res.send("API is running...");
